1c7ff17044f7d39e2b3e741ac6fc340d648c867c
BADNAM XD <yadavxd746@gmail.com>
2025-08-10 20:31:19 +0530

    Update mustjoin.py

--- SONALI_MUSIC/plugins/bot/mustjoin.py
+++ SONALI_MUSIC/plugins/bot/mustjoin.py
@@ -22,12 +22,12 @@ async def must_join_channel(app: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://files.catbox.moe/01imbu.jpg", caption=f"๏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [๏ sᴜᴘᴘᴏʀᴛ ๏]({link}) ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʜᴇᴀᴋ ᴍʏ ғᴇᴀᴛᴜʀᴇs.\n\nᴀғᴛᴇʀ ᴊᴏɪɴ ᴛʜᴇ [๏ ᴄʜᴀɴɴᴇʟ ๏]({link}) ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴀɴᴅ ᴛʏᴘᴇ /start ᴀɢᴀɪɴ !! ",
+                    photo="https://files.catbox.moe/fu6jk3.jpg", caption=f"๏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [๏ sᴜᴘᴘᴏʀᴛ ๏]({link}) ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʜᴇᴀᴋ ᴍʏ ғᴇᴀᴛᴜʀᴇs.\n\nᴀғᴛᴇʀ ᴊᴏɪɴ ᴛʜᴇ [๏ ᴄʜᴀɴɴᴇʟ ๏]({link}) ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴀɴᴅ ᴛʏᴘᴇ /start ᴀɢᴀɪɴ !! ",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("⌯ ᴊᴏɪɴ ⌯", url="https://t.me/lll_BADNAM_BABY_lll"),
-                                InlineKeyboardButton("⌯ ᴊᴏɪɴ ⌯", url="https://t.me/+Ius6mdxOqpFjNWM9"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/lll_BADNAM_BABY_lll"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/lll_BADNAM_BABY_lll"),
                             ]
                         ]
                     )
