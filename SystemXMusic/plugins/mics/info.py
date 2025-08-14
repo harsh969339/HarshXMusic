a57802ca9248ad8fbb6264a997073afcb0a50e23
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2025-08-14 10:31:56 +0530

    Update info.py

--- SystemXMusic/plugins/mics/info.py
+++ SystemXMusic/plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Jio_savan_music_bot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{app.username}?startgroup=true"),
     ],
 ]
 
@@ -73,7 +73,7 @@ INFO_TEXT = """
 ❍ ᴅᴄ ɪᴅ ▷ {}
 ❍ ʙɪᴏ ▷ {}
 
-❖ ᴍᴀᴅᴇ ʙʏ  ➛ [Kʀɪᴛɪ-ʙᴏᴛs](https://t.me/KRITI_SUPPORT_CHAT)
+❖ ᴍᴀᴅᴇ ʙʏ  ➛ [˹sʏsᴛᴇᴍ ʙᴏᴛs˼](https://t.me/FONT_CHANNEL_01)
 ▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
 
 """
